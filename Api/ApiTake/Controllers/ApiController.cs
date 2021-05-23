9a8aa8c80fa9b63eed29539a648f3b159e8c81e5
Cruzato <gabrielcruzato@gmail.com>
2021-05-23 19:14:33 -0300

    Initial commit

---
+++ Api/ApiTake/Controllers/ApiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiTake.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly string token;
+        
+        public ApiController(IConfiguration configuration)
+        {
+            token = configuration["GitHubToken"];
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var productInformation = new ProductHeaderValue("ApiTake");
+            var credentials = new Credentials(token);
+            var client = new GitHubClient(productInformation, new Uri("https://github.com/"))
+            { Credentials = credentials };
+
+            var request = new SearchRepositoriesRequest()
+            {
+                User = "Takenet",
+                Language = Language.CSharp
+            };
+
+            var result = await client.Search.SearchRepo(request);
+            var orderedByCreationDate = result.Items.OrderBy(x => x.CreatedAt);
+            return Ok(orderedByCreationDate.Take(5));
+        }
+    }
+}
